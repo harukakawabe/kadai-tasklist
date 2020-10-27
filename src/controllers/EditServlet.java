2a34641efbd43a6d2fb8762af0ccbd931bd89d38
haruka.kawabe <hrk.kwb12185738@gmail.com>
2020-10-27 10:55:12 +0900

    Modify null control

--- src/controllers/EditServlet.java
+++ src/controllers/EditServlet.java
@@ -46,6 +46,11 @@ public class EditServlet extends HttpServlet {
         request.setAttribute("task", t);
         request.setAttribute("_token", request.getSession().getId());
 
+        //タスクデータが存在している場合のみタスクIDをセッションスコープに登録
+        if(t!=null){
+            request.getSession().setAttribute("task_id",t.getId());
+        }
+
         // タスクIDをセッションスコープに登録
         request.getSession().setAttribute("task_id", t.getId());
 
